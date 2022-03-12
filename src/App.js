452ce64305be4edc8a7177a2109413cd2dc52c3c
songchuan <songchuan@pccpa.cn>
2022-03-12 10:23:36 +0800

    fix(test): 测试

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component{
             <div>
                 {/*<h1>你好</h1>*/}
                 {/*<h1>你好</h1>*/}
-                <Welcome name="123"/>
+                <Welcome name="1234"/>
             </div>
             // <div className="App">
             //     <header className="App-header">
